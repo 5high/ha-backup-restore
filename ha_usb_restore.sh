553e962a6215a3e027e9b79730a7e3db0a0b37cc
5high <5high.net@gmail.com>
2019-12-03 14:19:47 +0800

    Update ha_usb_restore.sh

--- ha_usb_restore.sh
+++ ha_usb_restore.sh
@@ -12,7 +12,8 @@ if [ -d "${BACKUP_FOLDER}" ]; then
                 log e "Homeassistant folder not found, is it correct?" 1
 	else
 	        sudo apt-get install zip -y
-		sudo rm -rf ${BACKUP_LOCATION}/ .cloud/ custom_components/ home-assistant* *.yaml .homekit.state .storage/
+		cd ${BACKUP_LOCATION}
+		sudo rm -rf .cloud/ custom_components/ home-assistant* *.yaml .homekit.state .storage/
 		unzip ${BACKUP_FOLDER}/hass-config.zip -d ${BACKUP_LOCATION}
 		sudo systemctl restart hass
 		sudo umount /media
